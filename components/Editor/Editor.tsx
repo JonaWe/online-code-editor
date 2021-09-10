bf7aaa14281e960780421f8f4fdd8a3a16bedaf5
Jona Wessendorf <jona.wessendorf1@uni-bielefeld.de>
2021-09-11 01:51:16 +0200

    adds Editor component

---
+++ components/Editor/Editor.tsx
@@ -0,0 +1,43 @@
+import { Controlled } from 'react-codemirror2';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material-palenight.css';
+// import 'codemirror/theme/dracula.css';
+// import 'codemirror/theme/material-darker.css';
+// import 'codemirror/theme/material.css';
+// import 'codemirror/theme/ayu-dark.css';
+
+if (typeof navigator !== 'undefined') {
+  require('codemirror/mode/xml/xml');
+  require('codemirror/mode/css/css');
+  require('codemirror/mode/javascript/javascript');
+  // require('codemirror/lib/codemirror.css');
+  // require('codemirror/theme/material.css');
+}
+
+interface EditorProps {
+  language: 'xml' | 'javascript' | 'css';
+  value: string;
+  onChange(value: string): void;
+}
+
+const Editor = ({ language, value, onChange }: EditorProps) => {
+  const handleChange = (editor: any, data: any, value: string) => {
+    onChange(value);
+  };
+  return (
+    <Controlled
+      value={value}
+      onBeforeChange={handleChange}
+      options={{
+        mode: language,
+        lineNumbers: true,
+        lineWrapping: true,
+        lint: true,
+        theme: 'material-palenight',
+      }}
+      className="code-mirror-wrapper"
+    />
+  );
+};
+
+export default Editor;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
 import styled from 'styled-components';
+import Editor from '../components/Editor';
 
 const Title = styled.h1`
   color: ${(props) => props.theme.colors.primary};
 `;
 
 const Home: NextPage = () => {
+  const [value, setValue] = useState('');
+  console.log(value);
+
   return (
     <>
       <Title>Titel</Title>
+      <Editor language="javascript" value={value} onChange={setValue} />
     </>
   );
 };
