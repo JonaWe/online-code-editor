6a09db7d8fb75d504707dd17b705e1dd03cebd72
Jona Wessendorf <jona.wessendorf1@uni-bielefeld.de>
2021-09-17 00:29:40 +0200

    adds a custom trigger for countdown start

--- hooks/useCountdown.ts
+++ hooks/useCountdown.ts
@@ -1,32 +1,42 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 
 interface useCountdownOptions {
   end?: number;
   onEnd?: () => any;
+  customStartTrigger?: boolean;
 }
 
 export default function useCountdown(
   start: number,
   options: useCountdownOptions
-): [number, boolean] {
+): [number, boolean, () => any] {
   const [counter, setCounter] = useState(start);
   const [done, setDone] = useState(false);
+  const [startTriggered, setStartTriggered] = useState(
+    !options.customStartTrigger
+  );
+
+  const triggerStart = () => {
+    setStartTriggered(true);
+  };
 
   useEffect(() => {
-    const end = options.end || 0;
-    if (counter > end) {
-      setTimeout(() => setCounter((current) => current - 1), 1000);
-    } else {
-      setDone(true);
+    if (startTriggered) {
+      const end = options.end || 0;
+      if (counter > end) {
+        setTimeout(() => setCounter((current) => current - 1), 1000);
+      } else {
+        setDone(true);
+      }
     }
-  }, [counter]);
+  }, [counter, options, startTriggered]);
 
+  // for the onEnd function
   useEffect(() => {
     if (done) {
       if (options.onEnd) options.onEnd();
     }
-  }, [done]);
+  }, [done, options]);
 
-  return [counter, done];
+  return [counter, done, triggerStart];
 }
